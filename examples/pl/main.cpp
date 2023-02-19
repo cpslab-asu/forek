f8276c81e72ec2d0e8843f09debc430a325917fa
Jacob Anderson <andersonjwan@gmail.com>
2023-02-18 22:47:27 -0700

    feat(examples): Update according to library

--- examples/pl/main.cpp
+++ examples/pl/main.cpp
@@ -1,8 +1,13 @@
 #include <pyforel/pyforel.hpp>
+#include <string>
+
+#include "pyforel/specification/pl/pl.hpp"
 
 using pyforel::specification::pl::PropositionalLogic;
 
 auto main() -> int {
-    auto spec = PropositionalLogic();
+    auto spec = PropositionalLogic("(p & q) -> r");
+    auto formula = spec.parse();
+
     return 0;
 }
