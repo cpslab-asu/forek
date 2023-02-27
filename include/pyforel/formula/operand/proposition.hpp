7c7fc4aa26133c61a17bab987b7a960d5b92398a
Jacob Anderson <andersonjwan@gmail.com>
2023-02-26 23:37:40 -0700

    feat: Define Proposition constructors

--- include/pyforel/formula/operand/proposition.hpp
+++ include/pyforel/formula/operand/proposition.hpp
@@ -2,9 +2,18 @@
 #define PYFOREL_FORMULA_OPERAND_PROPOSITION_HPP
 
 #include <pyforel/formula/operand/operand.hpp>
+#include <string>
+#include <utility>
 
 namespace pyforel::formula::operand {
-class Proposition : public Operand {};
+class Proposition : public Operand {
+   private:
+    std::string name_;
+
+   public:
+    Proposition() = delete;
+    explicit Proposition(std::string name) : name_(std::move(name)){};
+};
 }  // namespace pyforel::formula::operand
 
 #endif
