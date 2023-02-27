093eba40ca11778b5677c7713cb4ca023442ef9a
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 00:06:56 -0700

    style: Apply header formatting styles

--- include/pyforel/formula/operand/proposition.hpp
+++ include/pyforel/formula/operand/proposition.hpp
@@ -1,9 +1,10 @@
 #ifndef PYFOREL_FORMULA_OPERAND_PROPOSITION_HPP
 #define PYFOREL_FORMULA_OPERAND_PROPOSITION_HPP
 
-#include <pyforel/formula/operand/operand.hpp>
 #include <string>
 #include <utility>
+
+#include <pyforel/formula/operand/operand.hpp>
 
 namespace pyforel::formula::operand {
 class Proposition : public Operand {
