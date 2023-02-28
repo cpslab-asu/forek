934fd4437062aa57a69f1db572cee8d81b766270
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 23:25:02 -0700

    feat: Define PL specification

--- include/pyforel/specification/pl/pl.hpp
+++ include/pyforel/specification/pl/pl.hpp
@@ -1,10 +1,24 @@
 #ifndef PYFOREL_SPECIFICATION_PL_PL_HPP
 #define PYFOREL_SPECIFICATION_PL_PL_HPP
 
+#include <string>
+
+#include "pyforel/formula/formula.hpp"
 #include <pyforel/specification/specification.hpp>
 
 namespace pyforel::specification::pl {
-class PropositionalLogic : public Specification {};
+class PropositionalLogic : public Specification {
+   private:
+    std::string formula_;
+
+   public:
+    PropositionalLogic() = delete;
+    explicit PropositionalLogic(std::string formula)
+        : formula_(std::move(formula)) {}
+
+    /// Parse a PL formulae into a Formula.
+    [[nodiscard]] auto parse() const -> formula::Formula override;
+};
 }  // namespace pyforel::specification::pl
 
 #endif
