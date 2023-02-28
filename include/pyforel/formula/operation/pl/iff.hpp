3e84061c68257bab87db4615217bf0c2431adc99
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 21:40:27 -0700

    feat(pl): Implement iff node

--- include/pyforel/formula/operation/pl/iff.hpp
+++ include/pyforel/formula/operation/pl/iff.hpp
@@ -1,10 +1,20 @@
 #ifndef PYFOREL_FORMULA_OPERATION_PL_IFF_HPP
 #define PYFOREL_FORMULA_OPERATION_PL_IFF_HPP
 
+#include "pyforel/formula/core/node.hpp"
 #include <pyforel/formula/operation/binary.hpp>
 
 namespace pyforel::formula::operation::pl {
-class Iff : public Binary {};
+namespace kind {
+enum { Iff = 2 };
+}
+
+class Iff : public Binary {
+   public:
+    Iff() = delete;
+    Iff(const core::Node& lexpr, const core::Node& rexpr)
+        : Binary(kind::Iff, lexpr, rexpr) {}
+};
 }  // namespace pyforel::formula::operation::pl
 
 #endif
