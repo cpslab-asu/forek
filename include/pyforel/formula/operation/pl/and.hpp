6b06d017607674690934d529baef57ddb191f94a
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 21:40:12 -0700

    feat(pl): Implement and node

--- include/pyforel/formula/operation/pl/and.hpp
+++ include/pyforel/formula/operation/pl/and.hpp
@@ -1,10 +1,23 @@
 #ifndef PYFOREL_FORMULA_OPERATION_PL_AND_HPP
 #define PYFOREL_FORMULA_OPERATION_PL_AND_HPP
 
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include <pyforel/formula/core/node.hpp>
 #include <pyforel/formula/operation/binary.hpp>
 
 namespace pyforel::formula::operation::pl {
-class And : public Binary {};
-}  // namespace pyforel::formula::operation::pl
+namespace kind {
+enum { And = 1 };
+}
 
+class And : public Binary {
+   public:
+    And() = delete;
+    And(const core::Node& lexpr, const core::Node& rexpr)
+        : Binary(kind::And, lexpr, rexpr) {}
+};
+}  // namespace pyforel::formula::operation::pl
 #endif
