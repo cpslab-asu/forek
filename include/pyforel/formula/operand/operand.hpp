16032271f21b781c9f4f8463b7c9130fd06262d8
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 21:36:56 -0700

    feat: Implement derived operand-type node. This node appears only as leaves of the data structure.

--- include/pyforel/formula/operand/operand.hpp
+++ include/pyforel/formula/operand/operand.hpp
@@ -1,10 +1,29 @@
 #ifndef PYFOREL_FORMULA_OPERAND_OPERAND_HPP
 #define PYFOREL_FORMULA_OPERAND_OPERAND_HPP
 
+#include <memory>
+
 #include <pyforel/formula/core/node.hpp>
 
 namespace pyforel::formula::operand {
-class Operand : public core::Node {};
+class Operand : public core::Node {
+   public:
+    /// Create a clone of this object dynamically allocated.
+    ///
+    /// The resource created by this method is managed through the the standard
+    /// library smart pointer, so destructing it explicitly does not need to be
+    /// managed explicitly.
+    ///
+    /// Note: For any class deriving this class, it is required to re-define
+    /// this interface to support direct interfacing.
+    [[nodiscard]] inline auto clone() const
+        -> std::unique_ptr<core::Node> override {
+        return std::make_unique<Operand>(*this);
+    }
+
+    Operand() = delete;
+    explicit Operand(int kind) : core::Node(kind) {}
+};
 }  // namespace pyforel::formula::operand
 
 #endif
