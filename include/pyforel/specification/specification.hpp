63a14a595524ede756efc55ce013cf6a26f44368
Jacob Anderson <andersonjwan@gmail.com>
2023-02-27 23:24:31 -0700

    feat: Define base specification interface

--- include/pyforel/specification/specification.hpp
+++ include/pyforel/specification/specification.hpp
@@ -1,8 +1,14 @@
 #ifndef PYFOREL_SPECIFICATION_SPECIFICATION_HPP
 #define PYFOREL_SPECIFICATION_SPECIFICATION_HPP
 
+#include <pyforel/formula/formula.hpp>
+
 namespace pyforel::specification {
-class Specification {};
+class Specification {
+   public:
+    /// Parse a specification.
+    [[nodiscard]] virtual auto parse() const -> formula::Formula = 0;
+};
 }  // namespace pyforel::specification
 
 #endif
