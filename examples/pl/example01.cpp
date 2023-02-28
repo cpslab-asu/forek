01cd7ff0703efece78a840ee7e0b16a58e94af92
Jacob Anderson <andersonjwan@gmail.com>
2023-02-28 00:06:20 -0700

    feat: Update PL example to use specification

--- examples/pl/example01.cpp
+++ examples/pl/example01.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <utility>
 
+#include "pyforel/specification/pl/pl.hpp"
 #include <pyforel/formula/operand/proposition.hpp>
 #include <pyforel/formula/operation/pl/and.hpp>
 #include <pyforel/formula/operation/pl/iff.hpp>
@@ -14,12 +15,16 @@ using pyforel::formula::operation::pl::Iff;
 using pyforel::formula::operation::pl::Implies;
 using pyforel::formula::operation::pl::Not;
 using pyforel::formula::operation::pl::Or;
+using pyforel::specification::pl::PropositionalLogic;
 
 auto main() -> int {
     auto expr = Not(And(Proposition("p"),
                         Iff(Proposition("q"),
                             Implies(Proposition("r"),
                                     Or(Proposition("s"), Proposition("t"))))));
+
+    auto formula =
+        PropositionalLogic("!(p) and (q iff (r implies (s or t)))").parse();
 
     return 0;
 }
