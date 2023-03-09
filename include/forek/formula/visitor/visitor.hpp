741a20801833e0ae50d65f1732ab9c51f7cd4108
Jacob Anderson <andersonjwan@gmail.com>
2023-03-08 20:21:32 -0700

    feat: Add visitor interface

---
+++ include/forek/formula/visitor/visitor.hpp
@@ -0,0 +1,8 @@
+#ifndef FOREK_FORMULA_VISITOR_VISITOR_HPP
+#define FOREK_FORMULA_VISITOR_VISITOR_HPP
+
+namespace forek::formula::visitor {
+class Visitor {};
+}  // namespace forek::formula::visitor
+
+#endif
