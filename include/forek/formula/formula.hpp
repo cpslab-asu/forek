a16078f8b4cc2935e24e205391f6d9181b233caf
Jacob Anderson <andersonjwan@gmail.com>
2023-03-10 13:39:29 -0700

    feat: Update formula interface

--- include/forek/formula/formula.hpp
+++ include/forek/formula/formula.hpp
@@ -1,8 +1,30 @@
 #ifndef FOREK_FORMULA_FORMULA_HPP
 #define FOREK_FORMULA_FORMULA_HPP
 
+#include <memory>
+
+#include <forek/formula/core/node.hpp>
+#include <forek/formula/visitor/visitor.hpp>
+
 namespace forek::formula {
-class Formula {};
+class Formula {
+   private:
+    std::unique_ptr<core::Node> expr_;
+
+   public:
+    Formula() = delete;
+    explicit Formula(std::unique_ptr<core::Node> expr)
+        : expr_(std::move(expr)) {}
+
+    Formula(const Formula& other) : expr_(std::move(other.expr_->clone())){};
+    Formula(Formula&& other) : expr_(std::move(other.expr_)) {}
+
+    inline auto expr() -> std::unique_ptr<core::Node> {
+        return std::move(expr_);
+    }
+
+    auto evaluate(visitor::Visitor& visitor) -> void { expr_->accept(visitor); }
+};
 }  // namespace forek::formula
 
 #endif
