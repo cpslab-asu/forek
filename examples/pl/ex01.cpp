c1b82029ffa8ab35153a224e5d31ee5ac9c6623e
Jacob Anderson <andersonjwan@gmail.com>
2023-03-10 13:43:26 -0700

    feat: Update PL example

--- examples/pl/ex01.cpp
+++ examples/pl/ex01.cpp
@@ -13,33 +13,14 @@
 #include <forek/specification/pl/pl.hpp>
 
 using forek::examples::pl::ex01::PropositionalLogicSemantics;
-
-using forek::formula::core::operation::pl::And;
-using forek::formula::core::operation::pl::Or;
-
-using forek::formula::core::operand::pl::False;
-using forek::formula::core::operand::pl::Proposition;
-using forek::formula::core::operand::pl::True;
-
 using forek::specification::pl::PropositionalLogic;
 
 auto main() -> int {
-    auto p = Proposition("x");
-    auto req = And(
-        std::make_unique<Proposition>("p"),
-        std::make_unique<And>(std::make_unique<Proposition>("q"),
-                              std::make_unique<Or>(std::make_unique<True>(),
-                                                   std::make_unique<False>())));
-
     auto spec = PropositionalLogic("True & False -> (True | (False & True))");
     auto formula = spec.parse();
 
     auto visitor = PropositionalLogicSemantics();
-
-    std::cout << "Automated Visit:\n";
     formula.evaluate(visitor);
 
-    std::cout << "\nManual Visit:\n";
-    req.accept(visitor);
     return 0;
 }
