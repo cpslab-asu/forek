33b39b51a4e52ad330d09f803f77aed48ca811d7
Jacob Anderson <andersonjwan@gmail.com>
2023-03-10 12:58:17 -0700

    feat: Implement visitor pattern for semantic interfacing

---
+++ include/forek/formula/visitor/pl/visitor.hpp
@@ -0,0 +1,43 @@
+#ifndef FOREK_FORMULA_VISITOR_PL_VISITOR_HPP
+#define FOREK_FORMULA_VISITOR_PL_VISITOR_HPP
+
+#include <forek/formula/visitor/visitor.hpp>
+
+using VisitorInterface = forek::formula::visitor::Visitor;
+
+namespace forek::formula {
+namespace core {
+namespace operand::pl {
+// forward declarations
+class True;
+class False;
+class Proposition;
+}  // namespace operand::pl
+
+namespace operation::pl {
+class Not;
+class And;
+class Or;
+class Implies;
+class Iff;
+}  // namespace operation::pl
+}  // namespace core
+
+namespace visitor::pl {
+class Visitor : public VisitorInterface {
+   public:
+    virtual auto visit(const core::operation::pl::Not& ctx) -> void = 0;
+    virtual auto visit(const core::operation::pl::And& ctx) -> void = 0;
+    virtual auto visit(const core::operation::pl::Or& ctx) -> void = 0;
+    virtual auto visit(const core::operation::pl::Implies& ctx) -> void = 0;
+    virtual auto visit(const core::operation::pl::Iff& ctx) -> void = 0;
+
+    virtual auto visit(const core::operand::pl::True& ctx) -> void = 0;
+    virtual auto visit(const core::operand::pl::False& ctx) -> void = 0;
+    virtual auto visit(const core::operand::pl::Proposition& ctx) -> void = 0;
+};
+
+}  // namespace visitor::pl
+}  // namespace forek::formula
+
+#endif
--- include/forek/formula/visitor/visitor.hpp
+++ include/forek/formula/visitor/visitor.hpp
@@ -1,8 +1,43 @@
 #ifndef FOREK_FORMULA_VISITOR_VISITOR_HPP
 #define FOREK_FORMULA_VISITOR_VISITOR_HPP
 
-namespace forek::formula::visitor {
-class Visitor {};
-}  // namespace forek::formula::visitor
+#include <iostream>
+#include <stdexcept>
+
+namespace forek::formula {
+// forward declarations
+namespace core {
+class Node;
+
+namespace operand {
+class Operand;
+}
+
+namespace operation {
+class Operation;
+class Binary;
+class Unary;
+}  // namespace operation
+}  // namespace core
+
+namespace visitor {
+class Visitor {
+   public:
+    virtual ~Visitor() = default;
+
+    auto visit(const core::Node& ctx) -> void {}
+    auto visit(const core::operand::Operand& ctx) -> void {}
+    auto visit(const core::operation::Operation& ctx) -> void {}
+    auto visit(const core::operation::Binary& ctx) -> void {}
+    auto visit(const core::operation::Unary& ctx) -> void {}
+};
+
+class VisitorException : public std::runtime_error {
+   public:
+    explicit VisitorException(const std::string& msg)
+        : std::runtime_error(msg) {}
+};
+}  // namespace visitor
+}  // namespace forek::formula
 
 #endif
