bd8e34c68698fafec4c614c0c79923cee4c5424e
Jacob Anderson <andersonjwan@gmail.com>
2023-03-29 15:17:43 -0700

    style: Update line stretch

--- include/forek/formula/core/operand/arithmetic/integer.hpp
+++ include/forek/formula/core/operand/arithmetic/integer.hpp
@@ -1,7 +1,6 @@
 #ifndef FOREK_FORMULA_CORE_OPERAND_ARITHMETIC_INTEGER_HPP
 #define FOREK_FORMULA_CORE_OPERAND_ARITHMETIC_INTEGER_HPP
 
-#include <string>
 #include <typeinfo>
 
 #include <forek/formula/core/operand/operand.hpp>
@@ -20,20 +19,16 @@ class Integer : public Operand<T> {
 
     auto accept(visitor::Visitor<T>& visitor) -> void override {
         try {
-            this->data_ =
-                dynamic_cast<visitor::arithmetic::Visitor<T>&>(visitor).visit(
-                    *this);
+            this->data_ = dynamic_cast<visitor::arithmetic::Visitor<T>&>(visitor).visit(*this);
         } catch (const std::bad_cast&) {
             // A user error is thrown if a visitor (of a lower acceptance) is
             // attempted to be used (i.e., it is undefined behavior to downcast
             // an object that is not truly its casted type).
-            throw visitor::VisitorException(
-                "unable to visit Integer with provided visitor");
+            throw visitor::VisitorException("unable to visit Integer with provided visitor");
         }
     }
 
-    [[nodiscard]] inline auto clone() const
-        -> std::unique_ptr<core::Node<T>> override {
+    [[nodiscard]] inline auto clone() const -> std::unique_ptr<core::Node<T>> override {
         return std::make_unique<Integer>(value_);
     }
 
